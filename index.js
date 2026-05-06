d39c136fa5d46d2fedc95d729adf4d8f650dcb24
max antony janccokallo <izumi>
2026-05-06 09:38:57 -0500

    actualizacion servidor

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 const http = require('http')
 
-function requestController() {
+function requestController(req, res) {
+
     console.log('Bienvenidos al curso')
+
+    res.write('Bienvenidos al curso')
+    res.end()
+
 }
 
 const server = http.createServer(requestController)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
 server.listen(PORT, function () {
     console.log("Aplicacion corriendo en: " + PORT)
